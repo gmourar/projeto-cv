7003d6cc58fb17537f8c6d8de0eab28aba0b5eb8
Gabriel <gabrielmourarocha@gmail.com>
2023-05-09 10:52:45 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function aumentaImagem(certificadinho){
+	certificadinho.width = certificadinho.width+50;
+	certificadinho.height = certificadinho.height+50;			
+     }
+     function tamanhoNormal(certificadinho){
+        certificadinho.width = 150;
+        certificadinho.height = 30;			
+     }
